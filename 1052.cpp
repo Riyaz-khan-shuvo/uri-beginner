fe78ff30491c2219a0c1b47953ec04bee0639e0d
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-04-17 13:41:23 +0600

    1052 problem solved

---
+++ 1052.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int monthNumber;
+    cin >> monthNumber;
+    string month[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+    for (int i = 1; i <= sizeof(month) / sizeof(month[0]); i++)
+    {
+        if (monthNumber == i)
+        {
+            cout << month[i - 1] << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
