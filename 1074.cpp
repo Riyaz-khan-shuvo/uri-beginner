a7c076ef0aa554276fc5fa1c8fee3bd2f6d05d6b
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-05-23 10:47:09 +0600

    1074 has done

---
+++ 1074.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x, a;
+    
+    cin >> x;
+    for(int i = 0; i < x; i++){
+            cin >> a;
+            if(a == 0){
+                 cout << "NULL\n";
+            }
+            else{
+            if(a%2 == 0) cout << "EVEN";
+            else cout << "ODD";
+            
+            if(a<0) cout << " NEGATIVE\n";
+            else cout << " POSITIVE\n";
+            }
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+
+//     scanf("%d" , &n);
+
+//     if (n>0)
+//     {
+//         if (n%2==0)
+//         {
+//             printf("EVEN POSITIVE\n");
+//         }
+//         else
+//         {
+//             printf("ODD POSITIVE\n");
+//         }
+//     }
+//     else if (n<0)
+//     {
+//          if (n%2==0)
+//         {
+//             printf("EVEN NEGATIVE\n");
+//         }
+//         else
+//         {
+//             printf("ODD NEGATIVE\n");
+//         }
+//     }
+//     else
+//     {
+//         printf("NULL\n");
+//     }
+
+//       return 0;
+// }
